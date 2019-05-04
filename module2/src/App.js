3740f0b83ea105776cc66f262bcbb9daeae8810f
Gabriel Petrovick <petrovickg@hotmail.com>
2019-05-04 14:37:11 -0300

    Styled Component

---
+++ module2/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <div className="App" />
+  </Fragment>
+);
+
+export default App;
