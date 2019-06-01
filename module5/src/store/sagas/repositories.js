be035e6cfbf3235e769dfbee3969540d9b330d84
Petrovick Ubuntu <petrovickg@hotmail.com>
2019-06-01 13:40:51 -0300

    Module 6 spotify

--- module5/src/store/sagas/repositories.js
+++ module5/src/store/sagas/repositories.js
@@ -1,13 +1,11 @@
 import { call, put } from "redux-saga/effects";
-import axios from "axios";
+import api from "../../services/api";
+
 import { Creators as RepositoriesActions } from "../ducks/repositories";
 
 export function* getRepositories() {
     try {
-        const response = yield call(
-            axios.get,
-            "https://api.github.com/users/petrovick/repos"
-        );
+        const response = yield call(api.get, "users/petrovick/repos");
         yield put(RepositoriesActions.getSuccess(response.data));
     } catch (err) {
         yield put(RepositoriesActions.getFailure());
